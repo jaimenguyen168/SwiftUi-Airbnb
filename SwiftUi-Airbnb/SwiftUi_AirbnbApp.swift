4daa9fb0dfeda3cc78195f4e0b86753889dedc69
jaimenguyen168 <77992599+jaimenguyen168@users.noreply.github.com>
2024-03-07 16:39:57 -0500

    Initial Commit

---
+++ SwiftUi-Airbnb/SwiftUi_AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_AirbnbApp.swift
+//  SwiftUi-Airbnb
+//
+//  Created by Dat Nguyen on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
